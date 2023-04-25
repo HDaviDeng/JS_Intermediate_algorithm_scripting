42d86a233f4a59dffdaecdf284ede874c4ef4b2e
Ririhanna <112195420+Ririhanna@users.noreply.github.com>
2023-04-25 12:43:17 -0400

    Thanks for the link its time to read some algorithms to get prepared.

---
+++ sumAllPrimes.js
@@ -0,0 +1,14 @@
+/**
+ * A prime number is a whole number greater than 1 with exactly two divisores: 1 and itself. For example, 2 is a primenumber because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+ * 
+ * Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+ */
+
+//Problem Explanation: For this problem will find all prime numbers up to the numbers up to the number you are given as a parameter and return their sum. It is a good idea to research algorithms for finding prime numbers.
+
+/**
+ * Hint 1:
+ * You can use the definition of prime numbers or try learning and implementing your own Sieve of Eratosthenes. 
+ * Hint 2:
+ * This problem can be hard if you create your own code to check for primes, so don't feel bad if you use someone's algorithm for that part. Research is an important part of coding!
+ */
